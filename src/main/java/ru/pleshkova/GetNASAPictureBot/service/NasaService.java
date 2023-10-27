a57a85de671ab2cf3e74ea1a981a0d2bf80d2ccb
Anastasia <anastasiiapleshkova@gmail.com>
2023-10-27 14:51:10 +0300

    added menu

--- src/main/java/ru/pleshkova/GetNASAPictureBot/service/NasaService.java
+++ src/main/java/ru/pleshkova/GetNASAPictureBot/service/NasaService.java
@@ -1,7 +1,6 @@
 package ru.pleshkova.GetNASAPictureBot.service;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
@@ -12,40 +11,52 @@ import org.springframework.stereotype.Component;
 import ru.pleshkova.GetNASAPictureBot.config.NasaConfig;
 import ru.pleshkova.GetNASAPictureBot.model.NasaAnswer;
 
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Locale;
-import java.util.stream.Collectors;
+import java.util.Optional;
 
 @Slf4j
 @Component
 public class NasaService {
     private final NasaConfig nasaConfig;
     private final DateTimeFormatter formatterToNasa = DateTimeFormatter.ofPattern("yyyy-MM-dd", Locale.ENGLISH);
+    private final String FAILED_ANSWER = "Не получилось связаться с NASA";
     @Autowired
     public NasaService(NasaConfig nasaConfig) {
         this.nasaConfig = nasaConfig;
     }
 
-    public String getNasaUrl(LocalDateTime day) {
-        String mainAnswer;
+    private Optional<NasaAnswer> getAnswer(String day) {
         CloseableHttpClient client = HttpClients.createDefault();
         ObjectMapper objectMapper = new ObjectMapper();
+        CloseableHttpResponse response = null;
         try {
-            CloseableHttpResponse response = client.execute(new HttpGet(nasaConfig.getNasaUrl()+nasaConfig.getNasaKey()+"&date="+day.format(formatterToNasa)));
-            NasaAnswer answer = objectMapper.readValue(response.getEntity().getContent(), NasaAnswer.class);
-            mainAnswer = answer.getUrl();
+            response = client.execute(new HttpGet(nasaConfig.getNasaUrl()+nasaConfig.getNasaKey()+"&date="+day));
+            return Optional.ofNullable(objectMapper.readValue(response.getEntity().getContent(), NasaAnswer.class));
         } catch (IOException e) {
-            e.printStackTrace();
             log.info("Error during receiving answer from Nasa: " + e.getMessage());
-            mainAnswer = "не получилось(";
         }
-
-        return mainAnswer;
+        return Optional.empty();
     }
 
+    public String getNasaDayUrl (LocalDateTime day) {
+        Optional<NasaAnswer> answer = getAnswer(day.format(formatterToNasa));
+        if (answer.isPresent()) {
+            return answer.get().getUrl();
+        } else {
+            return FAILED_ANSWER;
+        }
+    }
+
+    public String getNasaDayDesc (LocalDateTime day) {
+        Optional<NasaAnswer> answer = getAnswer(day.format(formatterToNasa));
+        if (answer.isPresent()) {
+            return answer.get().getExplanation();
+        } else {
+            return FAILED_ANSWER;
+        }
+    }
 
 }
--- src/main/java/ru/pleshkova/GetNASAPictureBot/service/TelegramBot.java
+++ src/main/java/ru/pleshkova/GetNASAPictureBot/service/TelegramBot.java
@@ -4,8 +4,11 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.commands.SetMyCommands;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.commands.BotCommand;
+import org.telegram.telegrambots.meta.api.objects.commands.scope.BotCommandScopeDefault;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import ru.pleshkova.GetNASAPictureBot.config.BotConfig;
 
@@ -14,6 +17,8 @@ import java.time.LocalDateTime;
 import java.time.ZoneOffset;
 import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Locale;
 
 @Slf4j
@@ -27,6 +32,16 @@ public class TelegramBot extends TelegramLongPollingBot {
     public TelegramBot(BotConfig botConfig, NasaService nasaService) {
         this.botConfig = botConfig;
         this.nasaService = nasaService;
+        List<BotCommand> listCommands = new ArrayList<>();
+        listCommands.add(new BotCommand("/start", "Начать работу"));
+        listCommands.add(new BotCommand("/pic", "Показать картинку дня"));
+        listCommands.add(new BotCommand("/description", "Показать описание картинки дня"));
+        listCommands.add(new BotCommand("/info", "Помощь в работе с ботом"));
+        try {
+            this.execute(new SetMyCommands(listCommands, new BotCommandScopeDefault(), null));
+        } catch (TelegramApiException e) {
+            log.error("Error during adding bot commands: " + e.getMessage());
+        }
     }
     @Override
     public String getBotUsername() {
@@ -38,26 +53,34 @@ public class TelegramBot extends TelegramLongPollingBot {
     }
     @Override
     public void onUpdateReceived(Update update) {
-        System.out.println(update);
 
         if (update.hasMessage() && update.getMessage().hasText()) {
-            String messageText = update.getMessage().getText();
+            String messageText = update.getMessage().getText().toLowerCase();
             long chatId = update.getMessage().getChatId();
-            log.info("Receive from user "+ chatId + " : " + messageText);
-            if (messageText.equals("/start")) {
-                startCommandReceived(chatId, update.getMessage().getChat().getFirstName());
-            } else if (messageText.equals("/pic")) {
-                sendDayPicture(chatId, LocalDateTime.ofEpochSecond(update.getMessage().getDate(),0,ZoneOffset.UTC));
-            } else if (messageText.startsWith("/pic")) {
-                sendDayPicture(chatId, messageText.replaceFirst("/pic",""));
-            } else {
-                sendMessage(chatId, "Прости, я такого не умею");
+            String userName = update.getMessage().getChat().getFirstName();
+            log.info("Receive from user "+ chatId + ", name " + userName + " : " + messageText);
+            LocalDateTime today = LocalDateTime.ofEpochSecond(update.getMessage().getDate(), 0, ZoneOffset.UTC);
+
+            switch (messageText) {
+                case "/start" -> startCommandReceived(chatId, userName);
+                case "/pic" -> sendDayPicture(chatId, today);
+                case "/description" -> sendDescriptionOfDayPicture(chatId, today);
+                case "/info" -> sendMessage(chatId, "Чтобы получить сегодняшнюю картинку дня пришли /pic \n" +
+                        " Чтобы получить описание к картинке дня пришли /description \n" +
+                        "Если хочешь получить картинку другого дня пришли сообщение в формате, например, /pic17062023");
+                default -> {
+                    if (messageText.startsWith("/pic")) {
+                        sendDayPicture(chatId, messageText.replaceFirst("/pic", ""));
+                    } else {
+                        sendMessage(chatId, "Прости, я такого не умею");
+                    }
+                }
             }
         }
     }
     private void startCommandReceived(long chatId, String name){
         String answer = "Привет, " + name +"! Чтобы получить сегодняшнюю картинку дня пришли /pic \n" +
-                "Если хочешь получить картинку другого дня пришли сообщение в формате, например, /pic17062023";
+                "Больше информации в /info";
         sendMessage(chatId, answer);
     }
 
@@ -91,7 +114,12 @@ public class TelegramBot extends TelegramLongPollingBot {
     }
 
     private void sendDayPicture(long chatId, LocalDateTime day)  {
-        String textToSend = "Картинка дня " + day.format(formatterToClient) + " : \n " + nasaService.getNasaUrl(day);
+        String textToSend = "Картинка дня " + day.format(formatterToClient) + " : \n " + nasaService.getNasaDayUrl(day);
+        sendMessage(chatId, textToSend);
+    }
+
+    private void sendDescriptionOfDayPicture(long chatId, LocalDateTime day)  {
+        String textToSend = "Описание к картинке дня " + day.format(formatterToClient) + " : \n " + nasaService.getNasaDayDesc(day);
         sendMessage(chatId, textToSend);
     }
 
